83f3ec839235f8acc09c24d87444e541491528f3
christina araya <christina-araya@hotmail.com>
2015-02-04 05:57:13 +0000

    poetry

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,14 +1,20 @@
 class PoemsController < ApplicationController
+
+	def initialize
+	@poems = []
+	@poem = nil
+	end
 	
 	def index
-	@poems = []
-		Poems.each do |poem|
+		Poems.find_each do |poem|
 			poem.where(published: true)
 			@poems.push(poem)
 		end
 	end
 	
 	def show
+	
+	@poem = Poems.find(params[:id])
 	
 	end
 end
